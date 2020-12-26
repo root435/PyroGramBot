1544c8b5108c5aaa2e35fb2137daea2085109c95
root435 <76419787+root435@users.noreply.github.com>
2020-12-26 12:55:46 +0530

    Update ping.py

--- pyrobot/plugins/tools/ping.py
+++ pyrobot/plugins/tools/ping.py
@@ -9,7 +9,7 @@ from pyrobot import (
 from pyrobot.helper_functions.cust_p_filters import f_onw_fliter
 
 # -- Constants -- #
-ALIVE = "Not dead .."
+ALIVE = "Retards Never Die .."
 HELP = "CAADAgAD6AkAAowucAABsFGHedLEzeUWBA"
 REPO = ("User / Bot is available on GitHub:\n"
         "https://github.com/root435/test")
